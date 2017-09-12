ce16621162f0ac25933f62d6bfe053a089d16b7f
ludg <ludg@bc.edu>
2017-09-12 11:15:53 -0400

    part 2

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,9 +15,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    // MARK: - Functions
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: "sound\(soundNumber)") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: data in sound could not be played")
+            }
+        } else {
+            print("ERROR: sound file didn't load")
+        }
+    }
+    
+    func randomNum(previousNum: Int, upperBound: Int) -> Int {
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(upperBound)))
+        } while newIndex == previousNum
+        return newIndex
+    }
+    //MARK: - Properties
+    @IBOutlet weak var soundSwitch: UISwitch!
+    @IBOutlet weak var awesomeImage: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
-    var index = 0
+    
+    var awesomePlayer = AVAudioPlayer()
+    var index = -1
+    var imageNumber = -1
+    var soundNumber = -1
+    let numberofImages = 5
+    let numberofSounds = 3
+    
+    // MARK: - Actions
+    @IBAction func soundSwitchPressed(_ sender: UISwitch) {
+        if !soundSwitch.isOn && soundNumber != -1 {
+            //stop all sound immediately if there is sound playing and switch is pressed
+            awesomePlayer.stop()
+        }
+    }
     
     @IBAction func messageButton(_ sender: Any) {
         let messages = ["You Are Fantastic!",
@@ -26,38 +64,22 @@ class ViewController: UIViewController {
                         "I Am Loving It!",
                         "Thank You So Much!"]
         
-        var newIndex = -1
-        
-        repeat {
-            newIndex = Int(arc4random_uniform(UInt32(messages.count)))
-        } while index == newIndex
-        
-        index = newIndex
+        //show message
+        index = randomNum(previousNum: index, upperBound: messages.count)
         messageLabel.text = messages[index]
         
-        // var randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
-        // messageLabel.text = messages[randomIndex]
-        /* messageLabel.text = messages[index]
-        index += 1
+        //show image
+        awesomeImage.isHidden = false
+        imageNumber = randomNum(previousNum: imageNumber, upperBound: numberofImages)
+        awesomeImage.image = UIImage(named: "erlich\(imageNumber)")
         
-        
-        if index == messages.count {
-            index = 0
-        }*/
-        
-        // let message1 = "You Are Fantastic!"
-        // let message2 = "You Are Great!"
-        // let message3 = "You Are Amazing!"
-        //
-        // if messageLabel.text == message1 {
-        //   messageLabel.text = message2
-        // } else if messageLabel.text == message2{
-        //   messageLabel.text = message3
-        // else {
-        //   messageLabel.text = message1
-        // }
-    }
-    
+        //play audio
+        if soundSwitch.isOn {
+            soundNumber = randomNum(previousNum: soundNumber, upperBound: numberofSounds)
+            let soundName = "sound\(soundNumber)"
+            playSound(soundName: soundName, audioPlayer: &awesomePlayer)
+        }
 
+    }
 }
 
